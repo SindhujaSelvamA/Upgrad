f3a12149f824815567b0369aea27f3500700b55c
Sindhuja <sindhujaselvam227@gmail.com>
2022-01-27 16:53:02 +0700

    Add files via upload

---
+++ Assignment2_javascript.js
@@ -0,0 +1,15 @@
+
+
+
+var email = "abc@gmail.com";
+var password = 12345678;
+
+const e_mail = prompt("enter your email");
+const pass_word = prompt("enter your password");
+
+if ((e_mail = email) && (pass_word = password)) {
+    alert("Hurrah! you are loggin in");
+}
+else {
+    alert("try again");
+}
